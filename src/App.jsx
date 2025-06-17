a7b5320abda67798b0e350276845db19689a72a3
Mohamed Asad <mohamedasad11914@gmail.com>
2025-06-17 22:34:07 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>🧠 ArchiveVault Lite</h1>
+      <p>Add and manage your personal items</p>
+    </div>
+  );
+}
+
+export default App;
