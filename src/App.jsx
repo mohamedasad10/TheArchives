57f2804ce47138dd8cdf5cb1455ac67a9e3990ac
Mohamed Asad <mohamedasad11914@gmail.com>
2025-06-17 22:42:28 +0200

    form to add or edit an item (name, note, tag)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>🧠 ArchiveVault Lite</h1>
+      <h1>🧠 ArchiveVault </h1>
       <p>Add and manage your personal items</p>
     </div>
   );
